82a00317e1d0efe4468d4945ec64ea23ffdb78fe
Shiao Wang <109134157+wsasdsda@users.noreply.github.com>
2026-05-19 17:33:04 +0800

    Add files via upload

---
+++ PSMTrack/lib/test/evaluation/local.py
@@ -0,0 +1,18 @@
+from lib.test.evaluation.environment import EnvSettings
+
+def local_env_settings():
+    settings = EnvSettings()
+
+    # Set your local paths here.
+
+
+    # settings.fe240_path = ''
+    # settings.coesot_path = ''
+
+    settings.eventvot_path = ".../EventVOT/"
+    settings.prj_dir = '.../PSMTrack/'
+    settings.result_plot_path = settings.prj_dir + '/output/test/result_plots'
+    settings.results_path = settings.prj_dir + '/output/test/tracking_results'    # Where to store tracking results
+    settings.save_dir = settings.prj_dir + '/output'
+
+    return settings
